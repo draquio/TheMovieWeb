cdd71397a8b1c4bba73f9cdb75a9bd9430d52c36
draquio <draquio@gmail.com>
2023-12-07 17:29:26 -0400

    quitando mi api personal

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,7 @@
 export const ENV = {
-  Api_key: "1d0b6fe317a8169c108bc18f956a2c9c",
+  Api_key: "Tu-Api-key",
   Api_url: "https://api.themoviedb.org/3",
-  Token:
-    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZDBiNmZlMzE3YTgxNjljMTA4YmMxOGY5NTZhMmM5YyIsInN1YiI6IjY0ZDg3N2YwYmYzMWYyMDFjZTY5NDU3MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZFj_55YlP-2XjAd1CGtjQd_zhDzFM8ErKowqqEWjIiU",
+  Token:"Tu-Token",
   Api_image_url_original: "https://image.tmdb.org/t/p/original",
   Api_image_url_backgroud: "https://image.tmdb.org/t/p/w780",
   Api_image_url: "https://www.themoviedb.org/t/p/w220_and_h330_face",
